0bc9df0f5e3f15667cb7c4afd0122e9ea832700b
Alexander Sabanov <alexsab@mail.ru>
2023-08-08 00:44:39 +0400

    change and remove spaces

--- public/_plugins/page_filters.rb
+++ public/_plugins/page_filters.rb
@@ -1,24 +1,24 @@
 module Jekyll
-    module PageFilters
-      def children_of(all_pages, parent)
-        all_pages.select { |p| child_of?(p, parent) }
-      end
-  
-      private
-  
-      def child_of?(child, parent)
-        parent_path = parent["path"]
-        child_path = child.path
-  
-        # Exclude 'index.md' from becoming a child of itself
-        return false if parent_path == child_path
-  
-        # Remove 'index.md' from the parent path
-        parent_path = parent_path.split("index.html", 2).first
-  
-        child_path.start_with? parent_path
-      end
-    end
-  end
-  
-  Liquid::Template.register_filter(Jekyll::PageFilters)
+		module PageFilters
+			def children_of(all_pages, parent)
+				all_pages.select { |p| child_of?(p, parent) }
+			end
+
+			private
+
+			def child_of?(child, parent)
+				parent_path = parent["path"]
+				child_path = child.path
+
+				# Exclude 'index.md' from becoming a child of itself
+				return false if parent_path == child_path
+
+				# Remove 'index.md' from the parent path
+				parent_path = parent_path.split("index.html", 2).first
+
+				child_path.start_with? parent_path
+			end
+		end
+	end
+
+	Liquid::Template.register_filter(Jekyll::PageFilters)
